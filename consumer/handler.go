39a2a848111ef3a7d9e6e5f7e1a295f29c49a148
Aleksandr Kislitskiy <askislitskiy@avito.ru>
2018-09-11 09:44:56 +0300

    Consumer handler changed
    Cron tasks removed

--- consumer/handler.go
+++ consumer/handler.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
-
+	
+	"cloud.google.com/go/pubsub"
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/log"
 )
 
+type pushRequest struct {
+	Message      pubsub.Message
+	Subscription string
+}
+
 func push(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
 	log.Debugf(ctx, "Handling pushed message")
+	var req pushRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	log.Debugf(ctx, "Message: %v", req)
 	fmt.Fprintf(w, "ok")
 }
